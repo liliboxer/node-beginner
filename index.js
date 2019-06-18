0a974d66eee0152df895c6638967688ce9e7b5a1
Lili Boxer <ninaliboxer@gmail.com>
2019-06-18 16:30:31 -0700

    handle variable

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start(router.route);
+var handle = {};
+hand["/"] = requestHandlers.start;
+hand["/start"] = requestHandlers.start;
+hand["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + "received");
