07e6fe25102bb694333782103bcc60cdbfb9c7a9
Lili Boxer <ninaliboxer@gmail.com>
2019-06-18 17:49:32 -0700

    more refactoring

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 var querystring = require("querystring");
     fs = require("fs");
+    formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
     console.log("Request handler start was called");
 
     var body = `<html>` + 
@@ -24,9 +25,24 @@ function start(response, postData) {
 
 function upload(response, postData) {
     console.log("Request handler upload was called");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Yo've sent: " + querystring.parse(postData).text);
-    response.end();
+    
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+
+        fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+            if(error) {
+                fs.unlink("/tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+        
+        response.writeHead(200, {"Content-Type" : "text/html"});
+        response.write("received imageL <br/>>");
+        response.write("<img src='/show' />");
+        response.end();
+    })
 }
 
 
--- server.js
+++ server.js
@@ -3,21 +3,9 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + "received");
-        
-        request.setEncoding("utf8");
-
-        request.addEventListener("data", function(postdataChuk) {
-            postData += postdataChuk;
-            console.log("Received POST data chunk '" +
-            postdataChuk + "'.");
-        });
-
-        request.addEventListener("end", function() {
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, request);
     }
     
     http.createServer(onRequest).listen(8888);
